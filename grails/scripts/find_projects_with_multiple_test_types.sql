1fc09a3942bb4e8f50e607ed49c8df9307ae43f0
Todd Wells <ttopwells@gmail.com>
2009-11-28 13:27:24 -0800

    remove old sql upgrade scripts

--- grails/scripts/find_projects_with_multiple_test_types.sql
+++
@@ -1,7 +0,0 @@
--- Find projects whose test cases have more than one test_type.
--- NOTE: this should be ran before move_test_type_to_project.sql to avoid any unexpected data.
-
-SELECT * FROM project p WHERE 1 < (
-    SELECT COUNT(DISTINCT tc.test_type_id) FROM test_case tc
-    WHERE tc.project_id = p.id
-)
