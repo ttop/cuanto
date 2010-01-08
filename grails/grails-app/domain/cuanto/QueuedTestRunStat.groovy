24848cee3061345f48266f626964e2b3cc4123ac
Todd Wells <ttopwells@gmail.com>
2010-01-08 15:02:02 -0800

    add LGPL headers

--- grails/grails-app/domain/cuanto/QueuedTestRunStat.groovy
+++ grails/grails-app/domain/cuanto/QueuedTestRunStat.groovy
@@ -1,10 +1,23 @@
+/*
+	Copyright (c) 2010 Todd E. Wells
+
+	This file is part of Cuanto, a test results repository and analysis program.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package cuanto
-/**
- * User: Todd Wells
- * Date: Jan 5, 2010
- * Time: 12:53:22 PM
- * 
- */
 
 public class QueuedTestRunStat {
 	// This class represents a test run that is queued for stat calculation -- we'll just use the ID rather than
