dbe94ee7b3fbf7ebd1480bfdb7129caec3c06fd6
Todd Wells <ttopwells@gmail.com>
2010-01-30 20:09:18 -0800

    javadoc package info

---
+++ api/src/main/java/cuanto/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+*
+*		The Cuanto Java API provides a programmatic interface for creating, updating, deleting and querying for Cuanto
+*		test objects. It allows you to submit test results to the Cuanto server and retrieve results from the Cuanto
+*		server. The primary point of usage is the {@link cuanto.api.CuantoClient}.
+*
+*/
+package cuanto.api;
