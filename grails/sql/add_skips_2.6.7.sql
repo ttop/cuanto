a7665560e9f56877f520c7769215a40b162b3c09
Todd Wells <ttopwells@gmail.com>
2010-08-21 06:36:05 -0700

    add_skips sql file contents.

---
+++ grails/sql/add_skips_2.6.7.sql
@@ -0,0 +1,6 @@
+ALTER TABLE test_result ADD COLUMN is_skip BIT(1) NOT NULL;
+UPDATE test_result SET is_skip = TRUE, is_failure = FALSE WHERE name like 'Skip';
+
+ALTER TABLE test_run_stats ADD COLUMN skipped INT(11) DEFAULT NULL;
+UPDATE test_run_stats SET skipped = 0;
+
