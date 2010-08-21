2422aacff1fcf2e786ab4654292b1492e9bdab1c
Todd Wells <ttopwells@gmail.com>
2010-08-21 13:15:32 -0700

    cosmetic changes to SQL script

--- grails/sql/add_skips_2.6.7.sql
+++ grails/sql/add_skips_2.6.7.sql
@@ -1,7 +1,7 @@
 ALTER TABLE test_result ADD COLUMN is_skip BIT(1) NOT NULL;
-UPDATE test_result SET is_skip = TRUE, is_failure = FALSE WHERE name like 'Skip';
+UPDATE test_result SET is_skip = TRUE, is_failure = FALSE WHERE name LIKE 'Skip';
 
 ALTER TABLE test_run_stats ADD COLUMN skipped INT(11) DEFAULT NULL;
 UPDATE test_run_stats SET skipped = 0;
 
-insert into queued_test_run_stat (version, date_created, test_run_id) select 0, curdate() + INTERVAL 2 DAY, id from test_run tr;
+INSERT INTO queued_test_run_stat (version, date_created, test_run_id) SELECT 0, CURDATE() + INTERVAL 2 DAY, id FROM test_run tr;
