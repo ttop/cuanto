7a2800101c48451a1c63f299800a61317ba6c8d5
Todd Wells <ttopwells@gmail.com>
2010-08-21 11:12:55 -0700

    SQL to queue all existing test runs for recalculation

--- grails/sql/add_skips_2.6.7.sql
+++ grails/sql/add_skips_2.6.7.sql
@@ -4,3 +4,4 @@ UPDATE test_result SET is_skip = TRUE, is_failure = FALSE WHERE name like 'Skip'
 ALTER TABLE test_run_stats ADD COLUMN skipped INT(11) DEFAULT NULL;
 UPDATE test_run_stats SET skipped = 0;
 
+insert into queued_test_run_stat (version, date_created, test_run_id) select 0, curdate() + INTERVAL 2 DAY, id from test_run tr;
