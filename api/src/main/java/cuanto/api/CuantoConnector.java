348917f6b3e68c885bcc07cadf709744ecb52d6f
Todd Wells <ttopwells@gmail.com>
2010-08-25 17:08:06 -0700

    Change version to 2.6.6

--- api/src/main/java/cuanto/api/CuantoConnector.java
+++ api/src/main/java/cuanto/api/CuantoConnector.java
@@ -52,7 +52,7 @@ public class CuantoConnector {
 	 * deserialization.
 	 */
 	public final static String JSON_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
-	private final static String HTTP_USER_AGENT = "Java CuantoConnector 2.6.5; Jakarta Commons-HttpClient/3.1";
+	private final static String HTTP_USER_AGENT = "Java CuantoConnector 2.6.6; Jakarta Commons-HttpClient/3.1";
 
 	private static final String HTTP_GET = "get";
 	private static final String HTTP_POST = "post";
