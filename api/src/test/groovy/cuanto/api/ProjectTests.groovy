79d15b9ee994d96e31b098771c8398e1896c829b
Todd Wells <ttopwells@gmail.com>
2011-10-09 09:19:10 -0700

    add missing GPL headers

--- api/src/test/groovy/cuanto/api/ProjectTests.groovy
+++ api/src/test/groovy/cuanto/api/ProjectTests.groovy
@@ -1,12 +1,28 @@
+/*
+
+Copyright (c) 2011 Todd Wells
+
+This file is part of Cuanto, a test results repository and analysis program.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 package cuanto.api
 
 import cuanto.base.TestBase
 
-/**
- * User: Todd Wells
- * Date: 9/26/11
- * Time: 8:33 PM
- */
 class ProjectTests extends TestBase {
 
 	CuantoConnector client
