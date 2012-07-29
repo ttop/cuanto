4b0849f51d7f62e33a03bcba9dd32218b8018ecb
Todd Wells <ttopwells@gmail.com>
2012-07-29 07:58:10 -0700

    Initialize TestCase.analysisCount with a SQL script

---
+++ grails/sql/upgrade_to_2.9.0.sql
@@ -0,0 +1,5 @@
+-- Initialize TestCase.analysisCount
+UPDATE test_case tc SET tc.analysis_count =
+    (SELECT COUNT(*) FROM test_outcome tout, analysis_state anlz
+          WHERE tout.test_case_id = tc.id AND anlz.is_analyzed IS TRUE AND tout.analysis_state_id = anlz.id);
+
