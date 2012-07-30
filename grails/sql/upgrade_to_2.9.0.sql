cc1be5ded601b39fd68edb3f814ac3152c794753
Todd Wells <ttopwells@gmail.com>
2012-07-29 20:57:33 -0700

    Add test_case.analysis_count in db script

--- grails/sql/upgrade_to_2.9.0.sql
+++ grails/sql/upgrade_to_2.9.0.sql
@@ -1,6 +1,10 @@
+-- Add TestCase.analysisCount
+ALTER TABLE `test_case` ADD COLUMN `analysis_count` int(11);
+
 -- Initialize TestCase.analysisCount
 UPDATE test_case tc SET tc.analysis_count =
     (SELECT COUNT(*) FROM test_outcome tout, analysis_state anlz
           WHERE tout.test_case_id = tc.id AND anlz.is_analyzed IS TRUE AND tout.analysis_state_id = anlz.id);
 
+-- Add TestRun.allowPurge
 ALTER TABLE `test_run` ADD COLUMN `allow_purge` bit(1) default true;
